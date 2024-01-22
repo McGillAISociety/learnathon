93486707ba0fd9506989c6f336d14e86b8f75b1b
Alexa <90080367+itsAlexa@users.noreply.github.com>
2024-01-21 21:08:52 -0500

    Added the 2024 Schedule

--- components/Schedule.js
+++ components/Schedule.js
@@ -2,7 +2,14 @@ export default function Schedule() {
     return (
         <section>
             <h2>Schedule</h2>
-            <p>The Learnathon will be hosted at MILA from 2:00 PM - 6:00 PM on Friday, January 26th, 2024. <strong>Finalized schedule TBD.</strong></p>
+            <p>Note that there will be 10 minute breaks in between each talk.</p>
+            <ul>
+                <li><strong>2:00 PM - 2:40 PM:</strong> Laurence Perreault Levasseur</li>
+                <li><strong>2:50 PM - 3:30 PM:</strong> Liam Paull</li>
+                <li><strong>3:40 PM - 4:20 PM:</strong> David Rolnick</li>
+                <li><strong>4:30 PM - 5:10 PM:</strong> Steven Li</li>
+                <li><strong>5:20 PM - 6:00 PM:</strong> Yoshua Bengio</li>
+            </ul>
         </section>
     );
 }
